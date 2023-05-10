6e7fd5b3476370816a7dc546100fedf6eb4d9272
cts2032 <118189289+cts2032@users.noreply.github.com>
2023-05-10 11:23:13 +0900

    Delete src directory

--- src/api/api.js
+++
@@ -1,3 +0,0 @@
-const apiServer = "http://192.168.0.63:8000";
-
-export default apiServer;
--- src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import HomePageForm from "../components/HomePageForm/HomePageForm";
-
-const Home = () => {
-  return <HomePageForm />;
-};
-export default Home;
